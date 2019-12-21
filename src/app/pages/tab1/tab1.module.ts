85a913c722b367fce1563bd2ebbb869b784f4c58
Ariel Ezequiel Garrido <garridoar@outlook.com>
2019-12-21 08:38:07 -0300

    firs commit

--- src/app/pages/tab1/tab1.module.ts
+++ src/app/pages/tab1/tab1.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { DeseosService } from '../../services/deseos.service';
+import { Lista } from '../../model/lista.model';
 
 @NgModule({
   imports: [
@@ -14,4 +16,12 @@ import { Tab1Page } from './tab1.page';
   ],
   declarations: [Tab1Page]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule {
+
+
+  constructor(private deseosService: DeseosService) {
+  
+  }
+
+
+}
