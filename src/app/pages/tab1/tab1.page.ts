d4de0562b38e60d16303308c905c25ba9d50e3b7
Ariel <garridoariel2@hotmail.com>
2020-01-02 19:25:29 -0300

    almost finished

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Lista } from '../../model/lista.model';
 
 @Component({
   selector: 'app-tab1',
@@ -8,7 +11,57 @@ import { DeseosService } from '../../services/deseos.service';
 })
 export class Tab1Page {
 
-  constructor( public deseosService: DeseosService ) {
+  constructor( public deseosService: DeseosService,
+               private router: Router,
+               private alertController: AlertController ) {
+
+  }
+
+  async agregarLista() {
+
+    // this.router.navigateByUrl('/tabs/tab1/agregar');
+    
+      const alert = await this.alertController.create({
+        header: 'Nueva lista',
+        inputs: [
+          {
+            name: 'titulo',
+            type: 'text',
+            placeholder: 'Nombre de la lista'
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancelar',
+            role: 'cancel',
+            handler: () => {
+              console.log('Cancelar');
+            }
+          },
+          {
+            text: 'Crear',
+            handler: (data) => {
+              if(data.titulo.length === 0) {
+                return;
+              }
+              
+              // Crear lista
+              const listaId = this.deseosService.crearLista(data.titulo);
+
+              this.router.navigateByUrl(`/tabs/tab1/agregar/${listaId}`);
+
+            }
+          }
+        ]
+      });
+  
+      await alert.present();
+
+  }
+
+  listaSeleccionada( lista: Lista ) {
+
+    this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`);
 
   }
 
